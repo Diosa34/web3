5ae2d6b4b7ac2902fc42f22dc07c63985214ebbb
Diosa34 <superalex.osa@yandex.ru>
2022-12-12 19:38:29 +0300

    Added rendering points from DB; ResultBean implemented; added db configuration

--- src/main/java/com/github/diosa/web3/ResultsBean.java
+++ src/main/java/com/github/diosa/web3/ResultsBean.java
@@ -1,85 +1,83 @@
-//package com.github.diosa.web3;
-//
-//
-//import javax.annotation.PostConstruct;
-//import javax.annotation.Resource;
-//import javax.faces.bean.ApplicationScoped;
-//import javax.faces.bean.ManagedBean;
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import javax.transaction.*;
-//import java.io.Serializable;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@ManagedBean
-//@ApplicationScoped
-//public class ResultsBean implements Serializable {
-//    public List<Point> getResultPointsList() {
-//        return resultPointsList;
-//    }
-//
-//    public void setResultPointsList(List<Point> resultPointsList) {
-//        this.resultPointsList = resultPointsList;
-//    }
-//
-//    public void setNewPoint(Point newPoint) {
-//        this.newPoint = newPoint;
-//    }
-//
-//    private List<Point> resultPointsList;
-//
-//    public Point getNewPoint() {
-//        return newPoint;
-//    }
-//
-//    private Point newPoint = new Point();
-//    private Point lastPoint;
-//
-//    private final String PERSISTENCE_UNIT_NAME = "dot";
-//    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
-//    private EntityManager entityManager;
-//
-//    public ResultsBean() {
-//        this.resultPointsList = new ArrayList<>();
-//    }
-//
-//    public ResultsBean(ArrayList<Point> points) {
-//        this.resultPointsList = points;
-//    }
-//
-//    @Resource
-//    private UserTransaction userTransaction;
-//
-//    public List<Point> loadPointsFromDB() {
-//        return entityManager.createQuery("SELECT e FROM Points e").getResultList();
-//    }
-//
-//    public synchronized void addPointsToDB(Point point) throws Exception {
-//        userTransaction.begin();
-//        entityManager.persist(point);
-//        userTransaction.commit();
-//    }
-//
-//    @PostConstruct
-//    private void loadPoints() {
-//        this.resultPointsList = loadPointsFromDB();
-//    }
-//
-//    public synchronized void addPoint() {
-//        long startTime = System.nanoTime();
-//        this.newPoint.checkHit();
-//        this.newPoint.setCurrent(LocalDateTime.now());
-//        this.newPoint.setExec((System.nanoTime() - startTime) / 1000000d);
-//        try {
-//            addPointsToDB(this.newPoint);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        this.resultPointsList.add(0, this.newPoint);
-//        this.lastPoint = this.newPoint;
-//        this.newPoint = new Point();
-//    }
-//}
+package com.github.diosa.web3;
+
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.*;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ApplicationScoped
+public class ResultsBean implements Serializable {
+    public List<Point> getResultPointsList() {
+        return resultPointsList;
+    }
+
+    public void setResultPointsList(List<Point> resultPointsList) {
+        this.resultPointsList = resultPointsList;
+    }
+
+    public void setNewPoint(Point newPoint) {
+        this.newPoint = newPoint;
+    }
+
+    private List<Point> resultPointsList;
+
+    public Point getNewPoint() {
+        return newPoint;
+    }
+
+    private Point newPoint = new Point();
+
+    private final String PERSISTENCE_UNIT_NAME = "dot";
+    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
+    private EntityManager entityManager;
+
+    public ResultsBean() {
+        this.resultPointsList = new ArrayList<>();
+    }
+
+    public ResultsBean(ArrayList<Point> points) {
+        this.resultPointsList = points;
+    }
+
+    @Resource
+    private UserTransaction userTransaction;
+
+    public List<Point> loadPointsFromDB() {
+        return entityManager.createQuery("SELECT e FROM Points e").getResultList();
+    }
+
+    public synchronized void addPointsToDB(Point point) throws Exception {
+        userTransaction.begin();
+        entityManager.persist(point);
+        userTransaction.commit();
+    }
+
+    @PostConstruct
+    private void loadPoints() {
+        this.resultPointsList = loadPointsFromDB();
+    }
+
+    public synchronized void addPoint() {
+        long startTime = System.nanoTime();
+        this.newPoint.checkHit();
+        this.newPoint.setCurrent(LocalDateTime.now());
+        this.newPoint.setExec((System.nanoTime() - startTime) / 1000000d);
+        try {
+            addPointsToDB(this.newPoint);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.resultPointsList.add(0, this.newPoint);
+        this.newPoint = new Point();
+    }
+}
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -12,16 +12,13 @@ function addArea(r) {
               L 480 480" stroke="black" fill="#fc${areaFill}f""/>`
 }
 
-function setArea(checkbox) {
+function checkboxClick(checkbox){
     if (!checkbox.checked) {
         document.getElementById("g"+checkbox.id[6]).innerHTML = '';
     } else {
         addArea(checkbox.id[6]);
+        drawPointsFromDB(checkbox.id[6])
     }
-}
-
-function checkboxClick(checkbox){
-    setArea(checkbox);
 }
 
 function svgClick(event, svg, r) {
@@ -35,10 +32,9 @@ function svgClick(event, svg, r) {
     let y = -1 * (yPartOfSvg - 0.5) * 3 * r
     formClick(x, y)
 }
-
-
-function drawPoint(svg, x, y){
-    svg.innerHTML += "<circle cx='" + x + "' cy='" + y + "' r='7' fill='black'/>";
+// когда кликают по свг, то точки отрисовываются на свг, но точки из бд отрисовываются в группах, при чём при каждой смене радиуса
+function drawPoint(svgOrG, x, y, resultFill='black'){
+    svgOrG.innerHTML += `<circle cx="${x}" cy="${y}" r='7' fill="${resultFill}"/>`;
 }
 
 
@@ -49,4 +45,24 @@ function formClick(x, y){
     x_elem.value = x;
     y_elem.value = y;
     submit_elem.click();
+}
+
+let pointsList = document.querySelectorAll(".result-table td")
+
+function drawPointsFromDB(r){
+    for (let i = 10; i < pointsList.length; i += 10) {
+        if (pointsList[i+2+r].innerHTML === "true") {
+            let result = pointsList[i+8].innerHTML;
+            let x = Number(pointsList[i+1]);
+            let y = Number(pointsList[i+2]);
+            let fill = "FireBrick";
+            if (result.startsWith("In")) {
+                fill = "MediumAquamarine";
+            }
+            if (x <= 960 && y <= 960) {
+                drawPoint("g"+i, x, y, fill)
+            }
+        }
+    }
+//строчка для извлечения радиусов, в которые точка попала let rList = result.substring(result.indexOf(":") + 2, result.length()).split(" ");
 }
